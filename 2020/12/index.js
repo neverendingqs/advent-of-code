c251895f76f73e8064606d549d4bcbc4c78ffca2
Mark Tse <mark.tse@neverendingqs.com>
2020-12-20 22:37:13 -0500

    chore: 2020/12 - parsing input.

--- 2020/12/index.js
+++ 2020/12/index.js
@@ -1,3 +1,31 @@
-module.exports = async () => {
+const { promises: fs } = require('fs');
 
+async function getInput() {
+  return input = (await fs.readFile(`${__dirname}/input.txt`))
+    .toString()
+    .trim()
+    .split('\n')
+    .map(instruction => {
+      const action = instruction.substring(0, 1);
+      const value = parseInt(instruction.substring(1));
+      return { action, value };
+    });
+}
+
+async function p1() {
+  const input = await getInput();
+}
+
+async function p2() {
+}
+
+module.exports = async () => {
+  const [p1a, p2a] = await Promise.all([p1(), p2()]);
+  console.log('p1:', p1a)
+  console.log('p2:', p2a);
+
+  /*
+   * p1:
+   * p2:
+   */
 };
