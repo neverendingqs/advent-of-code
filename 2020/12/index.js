dc9c9d9a3713361c89573f4ce047a59e6a67e90c
Mark Tse <mark.tse@neverendingqs.com>
2020-12-21 15:27:25 -0500

    feat: 2020/12 part 1.

--- 2020/12/index.js
+++ 2020/12/index.js
@@ -1,5 +1,7 @@
 const { promises: fs } = require('fs');
 
+const DIRECTIONS = ['E', 'S', 'W', 'N'];
+
 async function getInput() {
   return input = (await fs.readFile(`${__dirname}/input.txt`))
     .toString()
@@ -14,6 +16,44 @@ async function getInput() {
 
 async function p1() {
   const input = await getInput();
+
+  let iDirection = 0;
+  const cumulative = DIRECTIONS.reduce(
+    (acc, direction) => Object.assign(acc, { [direction]: 0 }),
+    {}
+  );
+
+  for(const { action, value } of input) {
+    switch(action) {
+      case 'E':
+      case 'N':
+      case 'S':
+      case 'W':
+        cumulative[action] += value;
+        break;
+
+      case 'F':
+        cumulative[DIRECTIONS[iDirection]] += value;
+        break;
+
+      case 'L':
+        const newIDirection = iDirection - (value / 90);
+        iDirection = newIDirection < 0
+          ? newIDirection + 4
+          : newIDirection;
+          break;
+
+      case 'R':
+        iDirection = (iDirection + (value / 90)) % 4;
+        break;
+
+      default:
+        throw new Error('Should not have reached here.');
+    }
+  }
+
+  return Math.abs(cumulative['E'] - cumulative['W'])
+    + Math.abs(cumulative['N'] - cumulative['S']);
 }
 
 async function p2() {
