759b29a318f9fd2e07376e62b935e4cda8975ef2
Mark Tse <mark.tse@neverendingqs.com>
2020-12-23 14:21:10 -0500

    feat: 2020/14 part 2.

--- 2020/14/index.js
+++ 2020/14/index.js
@@ -42,7 +42,7 @@ async function p1() {
           value: BigInt(parseInt(value))
         };
       }
-    });;
+    });
 
   let memory = [];
   let currMask;
@@ -53,8 +53,8 @@ async function p1() {
       currMask = mask;
     } else {
       const { staticBits, wildcardBits } = currMask;
-      const unmaskedBits = (value & wildcardBits);
-      const finalValue = (staticBits | unmaskedBits);
+      const unmaskedBits = value & wildcardBits;
+      const finalValue = staticBits | unmaskedBits;
       memory[address] = finalValue;
     }
   }
@@ -69,7 +69,91 @@ async function p1() {
 }
 
 async function p2() {
-  const input = await getInput();
+  function maskToBitmasks(mask) {
+    const masks = [{ additionalBits: [], allowedBits: [], staticBits: [] }];
+
+    for(const bit of mask.split('')) {
+      if(bit === 'X') {
+        const numMasks = masks.length;
+        for(let i = 0; i < numMasks; i++) {
+          // We can push either a '0' or a '1' for both masks' `additionalBits`
+          // It doesn't matter as the one in `staticBits` takes precedence
+
+          const { additionalBits, allowedBits, staticBits } = masks[i];
+
+          // Clone the entry, but set the latest bit in `staticBits` differently
+          // This doubles the addresses to write to every time `X` appears in the mask (as desired)
+          masks.push({
+            additionalBits: [...additionalBits, '0'],
+            allowedBits: [...allowedBits, '0'],
+            staticBits: [...staticBits, '1']
+          });
+
+          additionalBits.push('0');
+          allowedBits.push('0');
+          staticBits.push('0');
+        }
+      } else {
+        masks.forEach(({ additionalBits, allowedBits, staticBits }) => {
+          additionalBits.push(bit);
+          allowedBits.push('1');
+          staticBits.push('0');
+        });
+      }
+    }
+
+    return masks.map(({ additionalBits, allowedBits, staticBits }) => ({
+      additionalBits: BigInt(
+        parseInt(additionalBits.join(''), 2)
+      ),
+      allowedBits: BigInt(
+        parseInt(allowedBits.join(''), 2)
+      ),
+      staticBits: BigInt(
+        parseInt(staticBits.join(''), 2)
+      )
+    }));
+  }
+
+  const input = (await getInput())
+    .map(line => {
+      if(line.startsWith('mask')) {
+        const mask = line.substring(7);
+        return { masks: maskToBitmasks(mask) };
+      } else {
+        const [, address, value] = line.match(/^mem\[(\d+)\] = (\d+)$/);
+        return {
+          address: BigInt(parseInt(address)),
+          value: BigInt(parseInt(value))
+        };
+      }
+    });
+
+  let memory = {};
+  let currMasks;
+  let i = 0;
+  for(const instruction of input) {
+    const { address, masks, value } = instruction;
+
+    if(masks) {
+      currMasks = masks;
+    } else {
+      currMasks.forEach(({ additionalBits, allowedBits, staticBits }) => {
+        const unmaskedBits = (address | additionalBits) & allowedBits;
+        const finalAddress = staticBits | unmaskedBits;
+        memory[finalAddress] = value;
+      });
+    }
+  }
+
+  return Object
+    .values(memory)
+    .filter(value => !!value)
+    .reduce(
+      (acc, value) => acc + value,
+      0n
+    )
+    .toString();
 }
 
 module.exports = async () => {
@@ -79,6 +163,6 @@ module.exports = async () => {
 
   /*
    * p1: 7440382076205
-   * p2:
+   * p2: 4200656704538
    */
 };
