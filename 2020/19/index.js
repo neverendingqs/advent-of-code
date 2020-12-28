8f8d4044e82ae7929414078895a5a139858101f2
Mark Tse <mark.tse@neverendingqs.com>
2020-12-28 14:53:41 -0500

    feat: 2020/19 part 1.

--- 2020/19/index.js
+++ 2020/19/index.js
@@ -1,13 +1,87 @@
 const { promises: fs } = require('fs');
 
 async function getInput() {
-  return (await fs.readFile(`${__dirname}/input.txt`))
+  const [rulesString, messagesString] = (await fs.readFile(`${__dirname}/input.txt`))
     .toString()
     .trim()
-    .split('\n');
+    .split('\n\n');
+
+  const rules = rulesString
+    .split('\n')
+    .reduce(
+      (acc, line) => {
+        const [index, rules] = line.split(':');
+        return Object.assign(acc, { [index]: rules.trim() });
+      },
+      {}
+    );
+
+  const messages = messagesString
+    .split('\n')
+    .map(line => line.split(''));
+
+  return { messages, rules };
 }
 
 async function p1() {
+  const { messages, rules } = await getInput();
+  const rulesEvaluator = Object
+    .entries(rules)
+    .reduce(
+      (acc, [index, ruleString]) => {
+        if(ruleString.startsWith('"')) {
+          const letter = ruleString[1];
+          return Object.assign(acc, {
+            [index]: function(message, iStart) {
+              return {
+                iEnd: iStart + 1,
+                isValid: letter === message[iStart]
+              };
+            }
+          })
+        }
+
+        const subrules = ruleString
+          .split('|')
+          .map(subruleString => subruleString.trim().split(' '));
+
+        return Object.assign(acc, {
+          [index]: function(message, iStart) {
+            for(let subrule of subrules) {
+              let areAllValid = true;
+              let i = iStart;
+
+              for(let ruleToCheck of subrule) {
+                const { iEnd, isValid } = this[ruleToCheck](message, i);
+                if(isValid) {
+                  i = iEnd;
+                } else {
+                  areAllValid = false;
+                  break;
+                }
+              }
+
+              if(areAllValid) {
+                return {
+                  iEnd: i,
+                  isValid: true
+                }
+              }
+            }
+
+            return { isValid: false };
+          }
+        })
+      },
+      {}
+    );
+
+  return messages
+    .filter(message => {
+      const { iEnd, isValid } = rulesEvaluator[0](message, 0);
+      return isValid && iEnd === message.length;
+    })
+    .length;
 }
 
 async function p2() {
@@ -19,7 +93,7 @@ module.exports = async () => {
   console.log('p2:', p2a);
 
   /*
-   * p1:
+   * p1: 198
    * p2:
    */
 };
