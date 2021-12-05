4d60ba8fe97a83e4e60b2c7e9be3a199dc2f2892
Mark Tse <mark.tse@neverendingqs.com>
2021-12-05 20:38:48 +0000

    feat: 2021/02 (completed).

--- 2021/src/aoc02/mod.rs
+++ 2021/src/aoc02/mod.rs
@@ -7,9 +7,13 @@ fn read_file() -> io::Lines<io::BufReader<File>> {
   io::BufReader::new(file).lines()
 }
 
-fn parse_line(line: io::Result<String>) -> (&str, i32) {
+fn parse_line<'a>(line: &'a io::Result<String>) -> (&'a str, i32) {
+  // Reason for `as_ref()`
+  // "move occurs because `*line` has type `Result<String, std::io::Error>`, which does not implement the `Copy` trait"
+  let unwrapped = line.as_ref().unwrap();
+
+  // Reason for `unwrapped` temp variable
   // "creates a temporary which is freed while still in use"
-  let unwrapped = line.unwrap();
   let parts: Vec<&str> = unwrapped.split_whitespace().collect();
   let direction = parts[0];
   let value = parts[1].parse::<i32>().unwrap();
@@ -22,7 +26,7 @@ fn p1() -> String {
   let mut depth = 0;
 
   for line in read_file() {
-    let (direction, value) = parse_line(line);
+    let (direction, value) = parse_line(&line);
     match direction {
       "forward" => h_pos += value,
       "down" => depth += value,
@@ -35,7 +39,24 @@ fn p1() -> String {
 }
 
 fn p2() -> String {
-  String::new()
+  let mut aim = 0;
+  let mut h_pos = 0;
+  let mut depth = 0;
+
+  for line in read_file() {
+    let (direction, value) = parse_line(&line);
+    match direction {
+      "forward" => {
+        h_pos += value;
+        depth += value * aim;
+      },
+      "down" => aim += value,
+      "up" => aim -= value,
+      _ => panic!("Unexpected direction '{}'", direction),
+    }
+  }
+
+  format!("{}", h_pos * depth)
 }
 
 pub fn answer() {
