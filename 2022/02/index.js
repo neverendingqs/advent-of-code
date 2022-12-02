2d699552264d71f0f1f0c5e77efbfb279b425abd
Mark Tse <mark.tse@neverendingqs.com>
2022-12-02 17:44:46 -0500

    feat(2022/02): p1.

--- 2022/02/index.js
+++ 2022/02/index.js
@@ -2,10 +2,48 @@ const { promises: fs } = require('fs');
 
 async function getInput() {
   const input = (await fs.readFile(`${__dirname}/input.txt`))
-    .toString();
+    .toString()
+    .trim();
+
+  return input
+    .split('\n')
+    .map(round => round.split(' '));
 }
 
 async function p1() {
+  const selectionScore = {
+    'X': 1, // rock
+    'Y': 2, // paper
+    'Z': 3, // scissors
+  };
+
+  const opponentToYourMove = {
+    'A': 'X', // rock
+    'B': 'Y', // paper
+    'C': 'Z', // scissors
+  }
+
+  const input = await getInput();
+  return input.reduce(
+    (acc, [opponentMove, myMove]) => {
+      let totalScore = acc + selectionScore[myMove];
+
+      if(opponentToYourMove[opponentMove] === myMove) {
+        return totalScore + 3;
+      }
+
+      if(
+        (opponentMove === 'A' && myMove === 'Y') ||
+        (opponentMove === 'B' && myMove === 'Z') ||
+        (opponentMove === 'C' && myMove === 'X')
+      ) {
+        return totalScore + 6;
+      }
+
+      return totalScore;
+    },
+    0
+  );
 }
 
 async function p2() {
