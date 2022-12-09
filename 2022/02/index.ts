c4d7afff9e57b120fbec2764e0851e0b22b36076
Mark Tse <mark.tse@neverendingqs.com>
2022-12-09 17:12:30 -0500

    feat(2022/02)

--- 2022/02/index.ts
+++ 2022/02/index.ts
@@ -1,15 +1,125 @@
 import readInput from '../lib/readInput';
 
-async function getInput(): Promise<any> {
+async function getInput(): Promise<string[][]> {
   const file = await readInput(__dirname);
+
+  return file
+    .split('\n')
+    .map(round => round.split(' '));
 }
 
 async function p1(): Promise<string> {
-  return '';
+  enum MyMove {
+    Rock = 'X',
+    Paper = 'Y',
+    Scissors = 'Z',
+  };
+
+  enum OpponentMove {
+    Rock = 'A',
+    Paper = 'B',
+    Scissors = 'C',
+  };
+
+  const selectionScore: { [key in MyMove]: number } = {
+    [MyMove.Rock]: 1,
+    [MyMove.Paper]: 2,
+    [MyMove.Scissors]: 3,
+  };
+
+  const opponentToYourMove: { [key in OpponentMove]: MyMove } = {
+    [OpponentMove.Rock]: MyMove.Rock,
+    [OpponentMove.Paper]: MyMove.Paper,
+    [OpponentMove.Scissors]: MyMove.Scissors,
+  };
+
+  const input = await getInput();
+
+  const score: number =  input.reduce(
+    (acc: number, [opponentMoveString, myMoveString]) => {
+      const opponentMove = opponentMoveString as OpponentMove;
+      const myMove = myMoveString as MyMove;
+
+      let totalScore = acc + selectionScore[myMove];
+
+      if(opponentToYourMove[opponentMove] === myMove) {
+        return totalScore + 3;
+      }
+
+      if(
+        (opponentMove === OpponentMove.Rock && myMove === MyMove.Paper) ||
+        (opponentMove === OpponentMove.Paper && myMove === MyMove.Scissors) ||
+        (opponentMove === OpponentMove.Scissors && myMove === MyMove.Rock)
+      ) {
+        return totalScore + 6;
+      }
+
+      return totalScore;
+    },
+    0
+  );
+
+  return score.toString();
 }
 
 async function p2(): Promise<string> {
-  return '';
+  enum Move {
+    Rock = 'A',
+    Paper = 'B',
+    Scissors = 'C',
+  };
+
+  enum EndState {
+    Lose = 'X',
+    Tie = 'Y',
+    Win = 'Z',
+  };
+
+  const selectionScore: { [key in Move]: number } = {
+    [Move.Rock]: 1,
+    [Move.Paper]: 2,
+    [Move.Scissors]: 3,
+  };
+
+  const loseMap: { [key in Move]: Move } = {
+    [Move.Rock]: Move.Scissors,
+    [Move.Paper]: Move.Rock,
+    [Move.Scissors]: Move.Paper,
+  };
+
+  const winMap: { [key in Move]: Move } = {
+    [Move.Rock]: Move.Paper,
+    [Move.Paper]: Move.Scissors,
+    [Move.Scissors]: Move.Rock,
+  };
+
+  const input = await getInput();
+
+  const solution = input.reduce(
+    (acc, [opponentMoveString, endStateString]) => {
+      const opponentMove = opponentMoveString as Move;
+      const endState = endStateString as EndState;
+
+      if(endState === EndState.Lose) {
+        const myMove = loseMap[opponentMove];
+        return acc + selectionScore[myMove];
+      }
+
+      if (endState === EndState.Tie) {
+        return acc + 3 + selectionScore[opponentMove];
+      }
+
+      if (endState === EndState.Win) {
+        const myMove = winMap[opponentMove];
+        return acc + 6 + selectionScore[myMove];
+      }
+
+      throw new Error('Should not reach here!');
+    },
+    0
+  );
+
+  return solution.toString();
 }
 
 export async function solution(): Promise<void> {
@@ -18,7 +128,7 @@ export async function solution(): Promise<void> {
   console.log('p2:', p2a);
 
   /*
-   * p1:
-   * p2:
+   * p1: 10941
+   * p2: 13071
    */
 };
