2a1a1f234ef5f1cd3477aa4b4aa074f82aa62f76
Mark Tse <mark.tse@neverendingqs.com>
2022-12-11 17:59:07 -0500

    feat(2022/03): p1.

--- 2022/03/index.ts
+++ 2022/03/index.ts
@@ -1,11 +1,53 @@
 import readInput from '../lib/readInput';
 
-async function getInput(): Promise<any> {
+async function getInput(): Promise<string[][]> {
   const file = await readInput(__dirname);
+  return file
+    .split('\n')
+    .map(bothInventories => {
+      if(bothInventories.length % 2 !== 0) {
+        throw new Error(`Invalid line '${bothInventories}'`);
+      }
+      const splitIndex = bothInventories.length / 2;
+      return [
+        bothInventories.slice(0, splitIndex),
+        bothInventories.slice(splitIndex),
+      ];
+    })
+}
+
+function getPriority(letter: string): number {
+  /*
+    * 'A'.charCodeAt(0); => 65
+    * 'a'.charCodeAt(0); => 97
+  */
+  const asciiValue: number = letter.charCodeAt(0);
+  if (asciiValue >= 97) {
+    // lower case
+    return asciiValue - 97 + 1;
+  }
+
+  // upper case
+  return asciiValue - 65 + 1 + 26;
 }
 
 async function p1(): Promise<string> {
-  return '';
+  const input: String[][] = await getInput();
+  const sumPriorities: number = input.reduce(
+    (acc, [firstCompartment, secondCompartment]) => {
+      const firstCompartmentSet = new Set(firstCompartment.split(''));
+      for(const item of secondCompartment.split('')) {
+        if(firstCompartmentSet.has(item)) {
+          return acc + getPriority(item);
+        }
+      }
+
+      throw new Error(`Could not find common item type: ${JSON.stringify({ firstCompartment, secondCompartment }, null, 2)}`);
+    },
+    0
+  );
+
+  return sumPriorities.toString();;
 }
 
 async function p2(): Promise<string> {
@@ -18,7 +60,7 @@ export async function solution(): Promise<void> {
   console.log('p2:', p2a);
 
   /*
-   * p1:
+   * p1: 8233
    * p2:
    */
 };
