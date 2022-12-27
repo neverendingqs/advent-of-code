a994c4640b434a65c5ae0f96cae19f5815ac9e30
Mark Tse <mark.tse@neverendingqs.com>
2022-12-27 16:07:27 -0500

    fix(2022): only trim trailing whitespace.

--- 2022/lib/readInput.ts
+++ 2022/lib/readInput.ts
@@ -3,5 +3,5 @@ import { promises as fs } from 'fs';
 export default async function(dir: string) {
   return (await fs.readFile(`${dir}/input.txt`))
     .toString()
-    .trim();
+    .trimEnd();
 }
