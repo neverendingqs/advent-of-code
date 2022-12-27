c1636562a77e8b34c6735b1a4d54d216609c65f1
Mark Tse <mark.tse@neverendingqs.com>
2022-12-27 16:31:07 -0500

    feat(2022/06): p1.

--- 2022/06/index.ts
+++ 2022/06/index.ts
@@ -1,11 +1,22 @@
 import readInput from '../lib/readInput';
 
-async function getInput(): Promise<any> {
+async function getInput(): Promise<string[]> {
   const file: string = await readInput(__dirname);
+  return file.split('');
 }
 
 async function p1(): Promise<string> {
-  return '';
+  const input: string[] = await getInput();
+  for(let i: number = 3; i < input.length; i++) {
+    const substring: string[] = input.slice(i - 3, i + 1);
+    const deduped: Set<string> = new Set(substring);
+
+    if([...deduped].join('') === substring.join('')) {
+      return (i + 1).toString();
+    }
+  }
+
+  throw new Error('Execution should not have reached here!');
 }
 
 async function p2(): Promise<string> {
@@ -18,7 +29,7 @@ export async function solution(): Promise<void> {
   console.log('p2:', p2a);
 
   /*
-   * p1:
+   * p1: 1723
    * p2:
    */
 }
