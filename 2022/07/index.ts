d2f5d8a31a16b0c120154b52fffcb7ad768fe913
Mark Tse <mark.tse@neverendingqs.com>
2022-12-31 19:49:11 +0000

    feat(2022/07): parsing input.

--- 2022/07/index.ts
+++ 2022/07/index.ts
@@ -1,10 +1,70 @@
 import readInput from '../lib/readInput';
 
+enum Command {
+  cd = 'cd',
+  ls = 'ls',
+}
+
+interface FileDetails {
+  isDirectory: boolean,
+  name: string,
+  size: number,
+}
+
+interface Exec {
+  arguments: string | undefined,
+  command: string,
+  output: FileDetails[],
+}
+
 async function getInput(): Promise<any> {
   const file: string = await readInput(__dirname);
+  const lines: string[] = file.split('\n');
+
+  const execs: Exec[] = [];
+  for(let i: number = 0; i < lines.length; i++) {
+    const [dollarSign, command, args]: string[] = lines[i].split(' ');
+    if(dollarSign !== '$') {
+      throw new Error(`Parsing error: ${JSON.stringify({ line: lines[i], i, execs })}`);
+    }
+
+    const exec: Exec = {
+      arguments: args,
+      command,
+      output: []
+    }
+
+    for(let j: number = i + 1; j < lines.length; j++) {
+      if(lines[j].startsWith('$')) {
+        break;
+      }
+
+      const [prefix, name]: string[] = lines[j].split(' ');
+      if(prefix === 'dir') {
+        exec.output.push({
+          isDirectory: true,
+          name,
+          size: 0,
+        });
+      } else {
+        exec.output.push({
+          isDirectory: false,
+          name,
+          size: parseInt(prefix),
+        });
+      }
+
+      i += 1;
+    }
+
+    execs.push(exec);
+  }
+
+  console.log(JSON.stringify(execs, null, 2))
 }
 
 async function p1(): Promise<string> {
+  await getInput();
   return '';
 }
 
