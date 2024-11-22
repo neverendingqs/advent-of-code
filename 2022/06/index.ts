73c1c8d8310482904fa9a4cd00a95a42e2e45974
Mark Tse <mark.tse@neverendingqs.com>
2024-11-22 16:55:08 -0500

    feat(2022/06) (#33)

--- 2022/06/index.ts
+++ 2022/06/index.ts
@@ -1,15 +1,31 @@
 import readInput from '../lib/readInput';
 
-async function getInput(): Promise<any> {
+async function getInput(): Promise<string[]> {
   const file: string = await readInput(__dirname);
+  return file.split('');
+}
+
+async function solve(numUniqueChars: number) {
+  const input: string[] = await getInput();
+  for(let i: number = numUniqueChars; i < input.length; i++) {
+    const substring: string[] = input.slice(i - numUniqueChars, i);
+    const deduped: string[] = [...new Set(substring)];
+
+    if(deduped.join('') === substring.join('')) {
+      return i.toString();
+    }
+  }
+
+  throw new Error('Execution should not have reached here!');
+
 }
 
 async function p1(): Promise<string> {
-  return '';
+  return solve(4);
 }
 
 async function p2(): Promise<string> {
-  return '';
+  return solve(14);
 }
 
 export async function solution(): Promise<void> {
@@ -18,7 +34,7 @@ export async function solution(): Promise<void> {
   console.log('p2:', p2a);
 
   /*
-   * p1:
-   * p2:
+   * p1: 1723
+   * p2: 3708
    */
 }
